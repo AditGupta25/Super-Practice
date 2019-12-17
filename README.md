2bca40d3fb9b8493579d1bd3ba0866b9c5c2e411
Adit Gupta <aditgupta1@yahoo.com>
2019-12-17 11:05:20 -0500

    true

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 <h6>The goal for this repo is to practice all the stuff out there</h6>
 
 1. 50-questions.py : This contains the implementation practice from the following link. https://www.byte-by-byte.com/wp-content/uploads/2019/01/50-Coding-Interview-Questions.pdf
+
+
+
+Things to look over:
+1) Stacks & Queues 
+2) BST 
+3) DO at least 1 recursion problem 
+4) BFS & DFS
+5) Find cycles in a graph
+6) 1 hard hashmap problem
+7) binary search on a sorted list
+8) Trie --> Done 
+9) Min an Max Heaps 
---
+++ trie.py
@@ -0,0 +1,30 @@
+class Trie: 
+	head = {}
+
+	def add(self, word):
+		cur = self.head
+
+		for ch in word:
+			if ch not in cur: 
+				cur[ch] = {}
+			cur = cur[ch]
+			cur["*"] = True
+
+	def search(self, word):
+		cur = self.head
+
+		for ch in word: 
+			if ch not in cur:
+				return False
+			cur = cur[ch]
+		if "*" in cur:
+			return True
+		else: 
+			return False 
+
+dictionary = Trie()
+dictionary.add("hi")
+dictionary.add("hello")
+print(dictionary.search("hello"))
+print(dictionary.search("test"))
+
